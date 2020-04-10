330679e4e049b8014f16cafccb39e7837f3c4745
SrdjanGaric <63466246+SrdjanGaric@users.noreply.github.com>
2020-04-10 19:59:24 +0200

    v0.1

---
+++ Rise/Assets/Scritps/MouseLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    Vector2 input;
+    float sensitivity = 2f;
+    float xRotation;
+
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Rotate();
+    }
+
+    void GetInput()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        input = new Vector2(mouseX, mouseY);
+    }
+
+    void Rotate()
+    {
+        xRotation -= input.y;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * input.x);
+    }
+}
