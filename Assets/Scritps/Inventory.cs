250d025eaca8f6a5bc87d93c0284568f1aabe3a3
SrdjanGaric <63466246+SrdjanGaric@users.noreply.github.com>
2020-05-08 12:43:16 +0200

    v0.3
    -Removed Character Creation due to bugs
    -Added Inventory System

---
+++ Assets/Scritps/Inventory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    bool inventoryEnabled;
+    public GameObject inventory;
+
+    int allSlots;
+    GameObject[] slot;
+
+    public GameObject slotHolder;
+
+    private void Start()
+    {
+        allSlots = 18;
+        slot = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            inventoryEnabled = !inventoryEnabled;
+        }
+
+        if (inventoryEnabled)
+        {
+            inventory.SetActive(true);
+        }
+        else
+        {
+            inventory.SetActive(false);
+        }
+    }
+}
